22e9339cd32769bdcdecc18c2170d63350f8a6f9
Tyler Funk <tyler.j.funk2@gmail.com>
2020-02-05 21:41:51 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -41,6 +41,12 @@ def sorted_matrix
   # Using Array literal syntax only, build another nested array that 
   # uses the arrays of conventional and organic produce as before.
   # However, this time, sort each internal array alphabetically by the first character
+  
+  [
+    ["Chiefs", "Patriots", "Ravens", "Titans"],
+    ["49ers", "Packers", "Seahawks", "Vikings"]
+]
+  
 end
 
 def matrix_lookup(matrix, row, column)
